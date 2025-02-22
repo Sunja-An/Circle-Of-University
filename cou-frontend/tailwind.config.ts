1bfe4ef40535a4bf4afcb584c55a4404c6a81703
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-02-23 00:11:00 +0900

    ✨ Feat: Logo asset added & CSS changed ([FE] COU Landing page #6)

--- cou-frontend/tailwind.config.ts
+++ cou-frontend/tailwind.config.ts
@@ -3,14 +3,31 @@ import type { Config } from "tailwindcss";
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/shared/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/widgets/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      xl: { max: "1200px" },
+      lg: { max: "1074px" },
+      md: { max: "768px" },
+      sm: { max: "640px" },
+    },
     extend: {
+      fontFamily: {
+        studioSans: ["studioSans"],
+        pretendard: ["pretendard-JP"],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        grayground: "var(--gray-background)",
+        primary: "var(--primary)",
+        secondary: "var(--secondary)",
+      },
+      backgroundImage: {
+        linearcolor: "linear-gradient(to bottom, #FFFFFF 66%, #FE9292)",
       },
     },
   },
