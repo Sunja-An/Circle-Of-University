db989f7c87f80801185411087615c8c9250047db
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-03-05 01:08:35 +0900

    🏷️  Feat: Add User Type & Circle Type ([MB] Mainpage UI & Login UI #8)

---
+++ cou-mobile/types/ROLE.d.ts
@@ -0,0 +1,6 @@
+// * User Role
+export type USER_ROLE = "NORMAL" | "ADMIN";
+
+// * Circle Role
+export type CIRCLE_TYPE = "CIRCLE" | "BUKATSU";
+export type CIRCLE_USER_ROLE = "NORMAL" | "SUBLEADER" | "LEADER";
