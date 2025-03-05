ea744500b2a24811f50cd664728591bfcd99284e
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-03-05 16:36:00 +0900

    ✨ Feat: FrontEnd & Server Directory deleted ([MB] Mainpage UI & Login UI #8)

---
+++ cou-mobile/app/(tabs)/profile.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function profile() {
+  return (
+    <View>
+      <Text>프로필 공간입니다.</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
