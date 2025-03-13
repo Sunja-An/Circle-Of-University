4d42237511c6f59e27717e7200380c4fee5a2fca
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-03-13 23:27:22 +0900

    ✨ Feat: Shared CSS added ([FE] COU Landing page #6)

--- cou-frontend/tailwind.config.ts
+++ cou-frontend/tailwind.config.ts
@@ -2,7 +2,6 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/shared/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/widgets/**/*.{js,ts,jsx,tsx,mdx}",
@@ -25,9 +24,24 @@ export default {
         grayground: "var(--gray-background)",
         primary: "var(--primary)",
         secondary: "var(--secondary)",
+        pink_0: "var(--pink-0)",
+        pink_1: "var(--pink-1)",
+        pink_2: "var(--pink-2)",
+        pink_3: "var(--pink-3)",
+        pink_4: "var(--pink-4)",
+        pink_5: "var(--pink-5)",
+        gray_0: "var(--gray-0)",
+        gray_1: "var(--gray-1)",
+        gray_2: "var(--gray-2)",
+        gray_3: "var(--gray-3)",
+        gray_4: "var(--gray-4)",
+        gray_5: "var(--gray-5)",
+        orange_0: "var(--orange)",
+        orange_1: "var(--orange-1)",
       },
       backgroundImage: {
         linearcolor: "linear-gradient(to bottom, #FFFFFF 66%, #FE9292)",
+        reverselinear: "linear-gradient(to top, #FFFFFF 66%, #FE9292)",
       },
     },
   },
